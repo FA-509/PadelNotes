015690e320fe0cce807e364f22c1e8e53122e32d
FA-509 <115668958+FA-509@users.noreply.github.com>
2025-10-07 17:40:16 +0100

    Add Import Game to API
    Add "Import a Game" POST method to API
    Enable submitting new game data to be stored in database.

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,7 @@
 const open_button = document.getElementById("importagame");
 const close_button = document.getElementById("close-button");
 const importagame_form = document.getElementById("importagame-form");
+const submit_button = document.getElementById("submit_button");
 
 open_button.addEventListener("click", () => {
   importagame_form.classList.add("open");
@@ -10,4 +11,28 @@ open_button.addEventListener("click", () => {
 
 close_button.addEventListener("click", () => {
   importagame_form.classList.remove("open");
+});
+
+submit_button.addEventListener("click", () => {
+  importagame_form.classList.remove("open");
+});
+
+// POST JSON to API
+
+const importgame_form = document.getElementById("importagameform");
+
+importgame_form.addEventListener("submit", (event) => {
+  const formData = new FormData(importgame_form);
+  const data = Object.fromEntries(formData);
+
+  fetch(
+    "https://padelnotes-functions-bta9hxdcajewb2cb.uksouth-01.azurewebsites.net/api/importgame",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
 });
