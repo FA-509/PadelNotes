f7841b8b012094703dbee2f9b588156784e73fda
FA-509 <115668958+FA-509@users.noreply.github.com>
2025-10-13 01:45:51 +0200

    Add dynamic table displaying full match history
    - Add FETCH function to fetch JSON from function and populate dynamic table

--- frontend/script.js
+++ frontend/script.js
@@ -34,3 +34,39 @@ importgame_form.addEventListener("submit", (event) => {
     body: JSON.stringify(data),
   });
 });
+
+// Full Match History Table
+
+fetch(
+  "https://padelnotes-function-app001.azurewebsites.net/api/full_match_history"
+)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (match_history) {
+    console.log(match_history);
+    let placeholder = document.querySelector("#table-data");
+    let out = "";
+    for (let match of match_history.data) {
+      out += `
+        <tr>
+          <td>${match.data.date}</td>
+          <td>${match.data.time}</td>
+          <td>${match.data.location}</td>
+          <td>${match.data.matchtype}</td>
+          <td>${match.data.teamaplayer1}</td>
+          <td>${match.data.teamaplayer2}</td>
+          <td>${match.data.teambplayer1}</td>
+          <td>${match.data.teambplayer2}</td>
+          <td>${match.data.set1scorea}</td>
+          <td>${match.data.set2scorea}</td>
+          <td>${match.data.set3scorea}</td>
+          <td>${match.data.set1scoreb}</td>
+          <td>${match.data.set2scoreb}</td>
+          <td>${match.data.set3scoreb}</td>
+        </tr>
+    `;
+    }
+    placeholder.innerHTML = out;
+  });
